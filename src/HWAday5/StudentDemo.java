7afa7d639cdfe147701bb6a299c1e275a4fb5e7b
tanxing123 <91770571+tanxing123@users.noreply.github.com>
2021-10-10 23:52:24 +0900

    Update StudentDemo.java

--- src/HWAday5/StudentDemo.java
+++ src/HWAday5/StudentDemo.java
@@ -6,7 +6,7 @@ public class StudentDemo {
 		student1.id =20210001;
 		student1.studentName = "王德住";
 		student1.birthday = "10-21";
-		student1.number = 159875462;
+		student1.number = "159875462";
 		student1.star = "白羊座";
 		student1.address = "松户站";
 		student1.gender = 0;
@@ -17,7 +17,7 @@ public class StudentDemo {
 		student2.id =20210002;
 		student2.studentName = "赵日升";
 		student2.birthday = "10-08";
-		student2.number = 159858422;
+		student2.number = "159858422";
 		student2.star = "狮子座";
 		student2.address = "新松户站";
 		student2.gender = 0;
@@ -28,7 +28,7 @@ public class StudentDemo {
 		student3.id =20210003;
 		student3.studentName = "王雨萌";
 		student3.birthday = "12-06";
-		student3.number = 159589462;
+		student3.number = "159589462";
 		student3.star = "双鱼座";
 		student3.address = "新宿站";
 		student3.gender = 1;
@@ -39,7 +39,7 @@ public class StudentDemo {
 		student4.id =20210004;
 		student4.studentName = "宇智波佐助";
 		student4.birthday = "05-21";
-		student4.number = 1289423644;
+		student4.number = "1289423644";
 		student4.star = "巨蟹座";
 		student4.address = "高田马场站";
 		student4.gender = 0;
@@ -50,7 +50,7 @@ public class StudentDemo {
 		student5.id =20210005;
 		student5.studentName = "春叶樱";
 		student5.birthday = "09-18";
-		student5.number = 159855864;
+		student5.number = "159855864";
 		student5.star = "金牛座";
 		student5.address = "涩谷站";
 		student5.gender = 1;
