3d1826fb2bb89522fa25c4a01ef7fa07e276a21a
tanxing123 <91770571+tanxing123@users.noreply.github.com>
2021-10-11 00:23:47 +0900

    Update AnimalDemo.java

--- src/Hwday6/AnimalDemo.java
+++ src/Hwday6/AnimalDemo.java
@@ -1,6 +1,6 @@
 public class AnimalDemo {
-    
-    public static void main(String[] args) {
+
+	public static void main(String[] args) {
 		
 		Animal animal1 = new Animal();
 		animal1.setName("考拉");
@@ -19,81 +19,77 @@ public class AnimalDemo {
 		System.out.println("------------");
 		Animal animal2 = new Animal();
 		animal2.setName("小白兔--大白");
-		System.out.println(animal1.getName());
+		System.out.println(animal2.getName());
 		animal2.setWeight(10D);
-		System.out.println(animal1.getWeight());
+		System.out.println(animal2.getWeight());
 		animal2.setColor("白色");
-		System.out.println(animal1.getColor());
+		System.out.println(animal2.getColor());
 		animal2.setLikefood("胡萝卜");
-		System.out.println(animal1.getLikefood());
+		System.out.println(animal2.getLikefood());
 		animal2.setbellyPocket("无");
-		System.out.println(animal1.getbellyPocket());
+		System.out.println(animal2.getbellyPocket());
 		animal2.setbellyPocket("短");
-		System.out.println(animal1.getlongEars());
+		System.out.println(animal2.getlongEars());
 		animal2.showInfo();
 		System.out.println("------------");
 		Animal animal3 = new Animal();
 		animal3.setName("小灰兔--大灰狼");
-		System.out.println(animal1.getName());
+		System.out.println(animal3.getName());
 		animal3.setWeight(15D);
-		System.out.println(animal1.getWeight());
+		System.out.println(animal3.getWeight());
 		animal3.setColor("灰色");
-		System.out.println(animal1.getColor());
+		System.out.println(animal3.getColor());
 		animal3.setLikefood("青菜");
-		System.out.println(animal1.getLikefood());
+		System.out.println(animal3.getLikefood());
 		animal3.setbellyPocket("无");
-		System.out.println(animal1.getbellyPocket());
+		System.out.println(animal3.getbellyPocket());
 		animal3.setbellyPocket("长");
-		System.out.println(animal1.getlongEars());
+		System.out.println(animal3.getlongEars());
 		animal3.showInfo();
 		System.out.println("------------");
 		Animal animal4 = new Animal();
 		animal4.setName("猫头鹰--秃头");
-		System.out.println(animal1.getName());
+		System.out.println(animal4.getName());
 		animal4.setWeight(20D);
-		System.out.println(animal1.getWeight());
+		System.out.println(animal4.getWeight());
 		animal4.setColor("黑色");
-		System.out.println(animal1.getColor());
+		System.out.println(animal4.getColor());
 		animal4.setLikefood("老鼠");
-		System.out.println(animal1.getLikefood());
+		System.out.println(animal4.getLikefood());
 		animal4.setbellyPocket("无");
-		System.out.println(animal1.getbellyPocket());
+		System.out.println(animal4.getbellyPocket());
 		animal4.setbellyPocket("短");
-		System.out.println(animal1.getlongEars());
+		System.out.println(animal4.getlongEars());
 		animal4.showInfo();
 		System.out.println("------------");
 		Animal animal5 = new Animal();
 		animal5.setName("猫头鹰--利眼");
-		System.out.println(animal1.getName());
+		System.out.println(animal5.getName());
 		animal5.setWeight(25D);
-		System.out.println(animal1.getWeight());
+		System.out.println(animal5.getWeight());
 		animal5.setColor("白灰色");
-		System.out.println(animal1.getColor());
+		System.out.println(animal5.getColor());
 		animal5.setLikefood("昆虫");
-		System.out.println(animal1.getLikefood());
+		System.out.println(animal5.getLikefood());
 		animal5.setbellyPocket("无");
-		System.out.println(animal1.getbellyPocket());
+		System.out.println(animal5.getbellyPocket());
 		animal5.setbellyPocket("短");
-		System.out.println(animal1.getlongEars());
+		System.out.println(animal5.getlongEars());
 		animal5.showInfo();
 		System.out.println("------------");
 		Animal animal6 = new Animal();
 		animal6.setName("猫头鹰--邬");
-		System.out.println(animal1.getName());
+		System.out.println(animal6.getName());
 		animal6.setWeight(23D);
-		System.out.println(animal1.getWeight());
+		System.out.println(animal6.getWeight());
 		animal6.setColor("紫黑色");
-		System.out.println(animal1.getColor());
+		System.out.println(animal6.getColor());
 		animal6.setLikefood("小鱼");
-		System.out.println(animal1.getLikefood());
+		System.out.println(animal6.getLikefood());
 		animal6.setbellyPocket("无");
-		System.out.println(animal1.getbellyPocket());
+		System.out.println(animal6.getbellyPocket());
 		animal6.setbellyPocket("短");
-		System.out.println(animal1.getlongEars());
+		System.out.println(animal6.getlongEars());
 		animal6.showInfo();
-		
-		
-
 	}
-
 }
