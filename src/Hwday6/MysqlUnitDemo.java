0b328c50ba94bc0b51315563ee4ab6cbc5bf33d6
tanxing123 <91770571+tanxing123@users.noreply.github.com>
2021-10-11 23:52:16 +0900

    Create MysqlUnitDemo.java

---
+++ src/Hwday6/MysqlUnitDemo.java
@@ -0,0 +1,16 @@
+public class MysqlUnitDemo {
+
+	public static void main(String[] args) {
+		
+		MysqlUnit a = new MysqlUnit();
+		a.getCoonect();
+		System.out.println("-----");
+		a.getClose();
+		
+		
+		
+		
+	}
+
+}
+
