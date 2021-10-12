a0a263d86588582062f0f6d8a0a2afdb28e44c97
tanxing123 <91770571+tanxing123@users.noreply.github.com>
2021-10-12 23:41:34 +0900

    Update Zouxin.java

--- src/Hwday6/Zouxin.java
+++ src/Hwday6/Zouxin.java
@@ -1,35 +1,41 @@
 import java.util.Scanner;
 
-public class Zhouxin {
+public class Zouxin {
 	/*求周薪
 	40小时以内，每小时50元
 	40－60小时，超出部分，每小时加薪20元
 	60－70小时，超出部分，每小时再加薪10元
 	大于70小时，一分不给（公司不建议超负荷工作）
 	要求，输入工作时长，输出周薪*/
+
+	final static double Hourywage = 50.0D;
+	private static double hours;
+	private static double newsalary;
+
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("请输入工作时长");
-		int time = scanner.nextInt();
-		 testPrice(time);
+		double hours = scanner.nextDouble();
+		double newsalary = getSalary(hours);
+		System.out.println("你的周薪:" + Zouxin.getSalary(hours));
+		
 		
 	}
-	private static void testPrice(int time) {
-	
-	
-		double newPrice = 0.0D;
-		if(time <= 40) {
-			newPrice = time * 50;
-		}else if(time > 40 && time <=60) {
-			newPrice = (time - 40) * 70 + (40*50);
-		}else if(time > 60 && time <= 70){
-			newPrice = (time - 60)* 80 + (40*50) + (20*70);
-		}else {
-			newPrice = 0;
-			System.out.println("公司不建议超负荷工作");
+	private static double getSalary(double hours) {
+		boolean flg = hours >70;
+		if(flg){
+			return newsalary = hours * Hourywage;
+		}else if(hours <= 40){
+newsalary = hours * Hourywage;
+		}else if(hours > 40 && hours <= 60){
+			newsalary = (40*Hourywage) + (hours - 40)*(Hourywage + 20.0);
+		}else if(hours > 60 && hours <=70){
+			newsalary = (40*Hourywage) + (20*(Hourywage + 20.0)) + (hours - 60)*(Hourywage + 20.0 +10.0);
+		}else{
+			newsalary = (40*Hourywage) + 20*70 + 10*80;
 		}
-		System.out.println("周薪:" + newPrice);
-		
+		return newsalary;
 		
 		
 	}
